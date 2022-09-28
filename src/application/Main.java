6d322cbde27b4206148706ad6240f0f38c4dc785
Michael Bernard <mich4el_bernard@hotmail.com>
2022-09-28 16:17:12 -0300

    MainView design

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,18 +4,20 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
-
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml")); // Instanciando e não chamando método estático; Importante para manipular a tela antes de carregar;
-			Parent parent = loader.load(); // O load carrega a View;
-			Scene mainScene = new Scene(parent); // Cena principal, já instanciando como arg o objeto principal da View (Anchor Pane);
+			ScrollPane scrollPane = loader.load(); // O load carrega a View;
+			
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
+			Scene mainScene = new Scene(scrollPane); // Cena principal, já instanciando como arg o objeto principal da View (Anchor Pane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
