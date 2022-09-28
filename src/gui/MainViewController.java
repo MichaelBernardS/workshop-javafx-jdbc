ecf70d17c09b9d99815d47dd0898448ee8492c6a
Michael Bernard <mich4el_bernard@hotmail.com>
2022-09-28 17:30:28 -0300

    Main View Controller

---
+++ src/gui/MainViewController.java
@@ -0,0 +1,41 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+public class MainViewController implements Initializable {
+	
+	@FXML
+	private MenuItem menuItemSeller;
+	
+	@FXML
+	private MenuItem menuItemDepartment;
+	
+	@FXML
+	private MenuItem menuItemAbout;
+	
+	// EventHandlers (Eventos que tratam os eventos do Menu);
+	
+	@FXML
+	public void onMenuItemSellerAction() { // Padrão usado nos EventHandlers. Começa com on, dps o nome do controle, dps o evento (Action é padrão) que vai tratar;
+		System.out.println("onMenuItemSellerAction");
+	}
+	
+	@FXML
+	public void onMenuItemDepartmentAction() {
+		System.out.println("onMenuItemDepartmentAction");
+	}
+	
+	@FXML
+	public void onMenuItemAboutAction() {
+		System.out.println("onMenuItemAboutAction");
+	}
+
+	@Override
+	public void initialize(URL uri, ResourceBundle rb) {	
+	}
+}
